cb0ce2484c7b55f275d0f4fcaeed796877dbbe0a
Isak Gabriel Chedid Girardello <isakgabrielcg@gmail.com>
2025-10-04 19:48:56 -0300

    ajuste

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,7 +28,6 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->registration() //adicionado paneas para demonstrar no render
             ->colors([
                 'primary' => Color::Amber,
             ])
