b6d7688cf72b6a4a689054ece3f088be61ded5f9
Isak Gabriel Chedid Girardello <isakgabrielcg@gmail.com>
2025-11-07 19:36:22 -0300

    Desativa a opção de depuração no ambiente e ajusta a busca global no painel administrativo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->globalSearch(false) // Disable global search
             ->colors([
                 'primary' => Color::Amber,
             ])
